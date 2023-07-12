2ed903001cf1a7bab4f3f3068178c300b73b7652
Diego Henrique <diego.reis.ruiz@gmail.com>
2023-07-12 07:06:38 -0300

    Teste chatbot não funcional

--- CodeSquadCRUD/src/br/com/tokio/model/Chatbot.java
+++ CodeSquadCRUD/src/br/com/tokio/model/Chatbot.java
@@ -2,19 +2,70 @@ package br.com.tokio.model;
 
 public class Chatbot {
 
-	private int id;	
-	private String message;
+	
+	String nome, cpf, dtNascimento, email, telefone;
 
-	public Chatbot( String message) {
-		this.message = message;
+	public Chatbot(String nome, String cpf, String dtNascimento, String email, String telefone) {
+		
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dtNascimento = dtNascimento;
+		this.email = email;
+		this.telefone = telefone;
+		
+	}
+	
+	
+
+	public Chatbot() {
+
+		//CONTRUTOR VAZIO
+		
 	}
 
-	public String getMessage() {
-		return message;
+
+
+	public String getNome() {
+		return nome;
 	}
 
-	public void setMessage(String message) {
-		this.message = message;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(String dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
 	
 }
